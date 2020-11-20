2cfcd9766e00da1febd2af509776377af9906b92
Aaron <60785649+Aaron-Storm@users.noreply.github.com>
2020-11-21 00:23:46 +0700

    Crash course JAM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import LinkList from './components/LinkList'
+import LinkForm from './components/LinkForm'
 
 function App() {
+
+  const [links, setLinks] = useState([]);
+  const loadLinks = async () => {
+
+    try {
+      const res = await fetch("/api/getLinks");
+      const links = await res.json();
+      setLinks(links);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    loadLinks();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-5">
+      <h1 className="text-center mb-5">List-o-Links</h1>
+        <LinkForm refreshLinks={loadLinks}/>
+        <LinkList links={links} refreshLinks={loadLinks}/>
     </div>
   );
 }
---
+++ src/components/LinkCard.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+function LinkCard({ link, refreshLinks }) {
+
+  const archiveLink = async () => {
+    link.archived = true;
+    try {
+      await fetch("/api/updateLink", {
+        method: "PUT",
+        body: JSON.stringify( link ),
+      });
+
+      refreshLinks();
+    } catch (err) {
+      console.error("oh no", err);
+    }
+  };
+
+  const deleteLink = async () => {
+    const id = link._id;
+    try {
+      await fetch("/api/deleteLink", {
+        method: "DELETE",
+        body: JSON.stringify({ id }),
+      });
+      refreshLinks();
+    } catch (err) {
+      console.error("oh no", err);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className="card-header">{link.name}</div>
+      <div className="card-header">
+        <a href={link.url}>{link.url}</a>
+        <p>{link.description}</p>
+      </div>
+      <div className="card-footer">
+        <button className="btn btn-warning mr-2" onClick={archiveLink}>
+          Archive
+        </button>
+        <button className="btn btn-danger" onClick={deleteLink}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default LinkCard;
